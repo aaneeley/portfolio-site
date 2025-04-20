bfee1331de868685fff6429de9c08353e6a8f1a1
Andrew <andrew.neeley@icloud.com>
2025-04-19 22:06:02 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,91 @@
+<svelte:head>
+    <title>Andrew Neeley | Portfolio</title>
+</svelte:head>
+
+<div class="fullscreen-bg"></div>
+
+<main class="w-screen h-screen flex justify-center items-center">
+    <div
+        class="max-w-md rounded-section relative custom-glass overflow-hidden p-10 pb-20 space-y-2 flex flex-col items-center"
+    >
+        <div class="avatar mb-6">
+            <div
+                class="ring-white ring-offset-white w-24 rounded-full ring ring-offset-2"
+            >
+                <img alt="avatar" src="/avatar1.jpeg" />
+            </div>
+        </div>
+        <h1>Andrew Neeley</h1>
+        <p class="text-center">
+            Full-stack software developer looking to join a team dedicated to
+            writing great software and creating incredible experiences.
+        </p>
+        <div
+            class="absolute bottom-0 flex flex-row space-x-6 justify-center items-center left-0 w-full bg-white h-17"
+        >
+            <button
+                class="btn btn-soft btn-circle rounded-full"
+                aria-label="asf"
+                onclick={() =>
+                    window.open("https://github.com/aaneeley", "_blank")}
+                ><svg
+                    width="20"
+                    height="20"
+                    viewBox="0 0 15 15"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M7.49933 0.25C3.49635 0.25 0.25 3.49593 0.25 7.50024C0.25 10.703 2.32715 13.4206 5.2081 14.3797C5.57084 14.446 5.70302 14.2222 5.70302 14.0299C5.70302 13.8576 5.69679 13.4019 5.69323 12.797C3.67661 13.235 3.25112 11.825 3.25112 11.825C2.92132 10.9874 2.44599 10.7644 2.44599 10.7644C1.78773 10.3149 2.49584 10.3238 2.49584 10.3238C3.22353 10.375 3.60629 11.0711 3.60629 11.0711C4.25298 12.1788 5.30335 11.8588 5.71638 11.6732C5.78225 11.205 5.96962 10.8854 6.17658 10.7043C4.56675 10.5209 2.87415 9.89918 2.87415 7.12104C2.87415 6.32925 3.15677 5.68257 3.62053 5.17563C3.54576 4.99226 3.29697 4.25521 3.69174 3.25691C3.69174 3.25691 4.30015 3.06196 5.68522 3.99973C6.26337 3.83906 6.8838 3.75895 7.50022 3.75583C8.1162 3.75895 8.73619 3.83906 9.31523 3.99973C10.6994 3.06196 11.3069 3.25691 11.3069 3.25691C11.7026 4.25521 11.4538 4.99226 11.3795 5.17563C11.8441 5.68257 12.1245 6.32925 12.1245 7.12104C12.1245 9.9063 10.4292 10.5192 8.81452 10.6985C9.07444 10.9224 9.30633 11.3648 9.30633 12.0413C9.30633 13.0102 9.29742 13.7922 9.29742 14.0299C9.29742 14.2239 9.42828 14.4496 9.79591 14.3788C12.6746 13.4179 14.75 10.7025 14.75 7.50024C14.75 3.49593 11.5036 0.25 7.49933 0.25Z"
+                        fill="currentColor"
+                    />
+                </svg>
+            </button>
+            <button
+                class="btn btn-soft btn-circle rounded-full"
+                aria-label="asf"
+                onclick={() =>
+                    window.open(
+                        "https://www.linkedin.com/in/aneeley/",
+                        "_blank",
+                    )}
+                ><svg
+                    width="20"
+                    height="20"
+                    viewBox="0 0 15 15"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M2 1C1.44772 1 1 1.44772 1 2V13C1 13.5523 1.44772 14 2 14H13C13.5523 14 14 13.5523 14 13V2C14 1.44772 13.5523 1 13 1H2ZM3.05 6H4.95V12H3.05V6ZM5.075 4.005C5.075 4.59871 4.59371 5.08 4 5.08C3.4063 5.08 2.925 4.59871 2.925 4.005C2.925 3.41129 3.4063 2.93 4 2.93C4.59371 2.93 5.075 3.41129 5.075 4.005ZM12 8.35713C12 6.55208 10.8334 5.85033 9.67449 5.85033C9.29502 5.83163 8.91721 5.91119 8.57874 6.08107C8.32172 6.21007 8.05265 6.50523 7.84516 7.01853H7.79179V6.00044H6V12.0047H7.90616V8.8112C7.8786 8.48413 7.98327 8.06142 8.19741 7.80987C8.41156 7.55832 8.71789 7.49825 8.95015 7.46774H9.02258C9.62874 7.46774 10.0786 7.84301 10.0786 8.78868V12.0047H11.9847L12 8.35713Z"
+                        fill="currentColor"
+                    />
+                </svg>
+            </button>
+            <button
+                class="btn btn-soft btn-circle rounded-full"
+                aria-label="asf"
+                onclick={() => window.open("mailto:andrew.neeley@icloud.com")}
+                ><svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M21.75 6.75V17.25C21.75 18.4926 20.7426 19.5 19.5 19.5H4.5C3.25736 19.5 2.25 18.4926 2.25 17.25V6.75M21.75 6.75C21.75 5.50736 20.7426 4.5 19.5 4.5H4.5C3.25736 4.5 2.25 5.50736 2.25 6.75M21.75 6.75V6.99271C21.75 7.77405 21.3447 8.49945 20.6792 8.90894L13.1792 13.5243C12.4561 13.9694 11.5439 13.9694 10.8208 13.5243L3.32078 8.90894C2.65535 8.49945 2.25 7.77405 2.25 6.99271V6.75"
+                        stroke="currentColor"
+                        stroke-width="2.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                    />
+                </svg>
+            </button>
+        </div>
+    </div>
+</main>
